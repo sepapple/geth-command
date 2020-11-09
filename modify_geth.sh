75cee1fa9341370e8b29fb50b7360067feae00e7
Infolab <infolab@Infolab1.local>
2020-11-09 16:58:21 +0900

    ポート等を修正した

--- modify_geth.sh
+++ modify_geth.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-geth --nodiscover --networkid "15" --port 30303  --rpc --rpcapi="eth,net,web3,personal,miner" --rpcaddr localhost --rpcport 8545 --datadir "~/private-net/" console 2>> ~/private-net/err.log --allow-insecure-unlock --unlock 0 --password ~/Test_command/password.txt 
+geth --nodiscover --networkid "15" --port 30304  --rpc --rpcapi="eth,net,web3,personal,miner" --rpcaddr localhost --rpcport 8546 --datadir "~/private-net/" console 2>> ~/private-net/err.log --allow-insecure-unlock --unlock 0 --password ~/Test_command/password.txt 
